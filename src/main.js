a3ba273d322423c68bdf01e4352130a10dbb97ca
fanfan <28298253734@qq.com>
2022-06-08 22:33:12 +0800

    第一次提交

--- src/main.js
+++ src/main.js
@@ -2,9 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Vant from 'vant'
+import 'amfe-flexible'
+import '@/styles/index.less'
+import 'vant/lib/index.css'
 
 Vue.config.productionTip = false
-
+Vue.use(Vant)
 new Vue({
   router,
   store,
